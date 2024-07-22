3b76c92068cb08e2d043ca80b92427afe3cf11a3
MatthewAraujo <matthewaraujo20@gmail.com>
2024-07-22 17:02:12 -0300

    feat: creating my filter exceptions

--- src/CashFlow.API/Controllers/ExpensesController.cs
+++ src/CashFlow.API/Controllers/ExpensesController.cs
@@ -14,26 +14,9 @@ namespace CashFlow.API.Controllers
         [HttpPost]
         public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
         {
-            try
-            {
-
-                var useCase = new RegisterExpenseUseCase();
-                var response = useCase.Execute(request);
-                return Created(string.Empty, response);
-            }
-            catch (ErrorOnValidationException ex) 
-            {
-                var errorResponse = new ResponseErrorJson(ex.Errors);
-
-                return BadRequest(errorResponse);
-                
-            }
-            catch
-            {
-                var errorResponse = new ResponseErrorJson("unknown error");
-
-                return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
-            }
+            var useCase = new RegisterExpenseUseCase();
+            var response = useCase.Execute(request);
+            return Created(string.Empty, response);
         }
     }
 }
---
+++ src/CashFlow.API/Filters/ExceptionFilter.cs
@@ -0,0 +1,44 @@
+﻿using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionBase;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CashFlow.API.Filters
+{
+    public class ExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            if (context.Exception is CashFlowException)
+            {
+                HandleProjectException(context);
+            }
+            else
+            {
+                ThrowUnkownError(context);
+
+            }
+        }
+
+        private void HandleProjectException(ExceptionContext context) 
+        {
+            if (context.Exception is ErrorOnValidationException)
+            {
+                var ex = (ErrorOnValidationException)context.Exception;
+
+                var errorResponse = new ResponseErrorJson(ex.Errors);
+                context.HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+                context.Result = new ObjectResult(errorResponse);
+            }
+        }
+        private void ThrowUnkownError(ExceptionContext context) 
+        {
+            var errorResponse = new ResponseErrorJson("uknown Error");
+
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            context.Result = new ObjectResult(errorResponse);
+
+        }
+
+    }
+}
