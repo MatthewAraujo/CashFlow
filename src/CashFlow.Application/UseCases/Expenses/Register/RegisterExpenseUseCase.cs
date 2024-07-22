633986ed86b8c510fa1111e6797f1ac080db436c
MatthewAraujo <matthewaraujo20@gmail.com>
2024-07-22 14:38:34 -0300

    feat: validations of register expenses

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -1,4 +1,5 @@
-﻿using CashFlow.Communication.Requests;
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
 using CashFlow.Communication.Responses;
 
 namespace CashFlow.Application.UseCases.Expenses.Register
@@ -7,8 +8,37 @@ namespace CashFlow.Application.UseCases.Expenses.Register
     {
         public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
         {
+            Validate(request);
+
             return new ResponseRegisteredExpenseJson();
 
+        }
+
+        private void Validate(RequestRegisterExpenseJson request)
+        {
+            var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+            if (titleIsEmpty)
+            {
+                throw new ArgumentException("The tile is required");
+            }
+
+            if (request.Amout <= 0)
+            {
+                throw new ArgumentException("The Amount must be grater than zero");
+            }
+
+            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+            if (result > 0)
+            {
+                throw new ArgumentException("Expenses cannot be in the future");
+            }
+
+            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+            if (!paymentTypeIsValid)
+            {
+                throw new ArgumentException("Payment Type is not valid");
+
+            }
         }
     }
 }
--- src/CashFlow.Communication/Responses/ResponseRegisteredExpenseJson.cs
+++ src/CashFlow.Communication/Responses/ResponseRegisteredExpenseJson.cs
@@ -3,5 +3,7 @@
     public class ResponseRegisteredExpenseJson
     {
 
+
     }
+
 }
