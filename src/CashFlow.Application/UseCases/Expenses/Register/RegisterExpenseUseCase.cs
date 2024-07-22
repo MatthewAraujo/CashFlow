5e6af2880804635c75d69ed2f8a0b611f191347b
MatthewAraujo <matthewaraujo20@gmail.com>
2024-07-22 15:48:16 -0300

    refactor: validation with Validator library

--- src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -16,29 +16,10 @@ namespace CashFlow.Application.UseCases.Expenses.Register
 
         private void Validate(RequestRegisterExpenseJson request)
         {
-            var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
-            if (titleIsEmpty)
-            {
-                throw new ArgumentException("The tile is required");
-            }
+            var validator = new RegisterExpenseValidator();
+            var result =  validator.Validate(request);
 
-            if (request.Amout <= 0)
-            {
-                throw new ArgumentException("The Amount must be grater than zero");
-            }
-
-            var result = DateTime.Compare(request.Date, DateTime.UtcNow);
-            if (result > 0)
-            {
-                throw new ArgumentException("Expenses cannot be in the future");
-            }
-
-            var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
-            if (!paymentTypeIsValid)
-            {
-                throw new ArgumentException("Payment Type is not valid");
-
-            }
+          
         }
     }
 }
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -0,0 +1,17 @@
+﻿using CashFlow.Communication.Requests;
+using FluentValidation;
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpenseJson>
+    {
+        public RegisterExpenseValidator() 
+        {
+            RuleFor(expense => expense.Title).NotEmpty().WithMessage("The tile is required");
+            RuleFor(expense => expense.Amout).GreaterThan(0).WithMessage("The Amount must be grater than zero");
+            RuleFor(expense => expense.Date).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Expenses cannot be in the future");
+            RuleFor(expense =>  expense.PaymentType).IsInEnum().WithMessage("Payment Type is not valid");
+        }
+
+    }
+}
