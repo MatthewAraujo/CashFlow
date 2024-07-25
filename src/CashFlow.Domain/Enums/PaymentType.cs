fc209934cdcc035647d4d808949c3a86ce3755ef
MatthewAraujo <matthewaraujo20@gmail.com>
2024-07-25 17:05:17 -0300

    yueses

---
+++ src/CashFlow.Domain/Enums/PaymentType.cs
@@ -0,0 +1,10 @@
+﻿namespace CashFlow.Domain.Enums
+{
+    public enum PaymentType
+    {
+        Cash = 0,
+        CreditCard = 1,
+        DebitCard = 2,
+        EletronicTransfer = 3,
+    }
+}
---
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess
+{
+    public class CashFlowDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "";
+            var serverVersion = new MySqlServerVersion(new Version(8,0,0));
+
+
+            optionsBuilder.UseMySql();
+        }
+    }
+}
